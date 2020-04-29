73fa7dfcc839882c852c7d692c82ae3b91652c70
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-04-29 01:50:24 -0300

    sets styles && adds api service

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios do Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/13472488?s=460&u=7c0fdc6888ff188daeaaa8b3f76afaea9cbb68b0&v=4"
+            alt="Marcelo Amorim"
+          />
+          <div>
+            <strong>marcelo-amorim/nodejs-concepts</strong>
+            <p>
+              Conceitos do Node.js. Desafio da Bootcamp GoStack da Rocketseat
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
